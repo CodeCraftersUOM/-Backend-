f2527f1db713f1791fc1b4e842fc03a117aac30b
chandupasasmitha <chandupasasmitha@gmail.com>
2025-05-27 12:04:09 +0530

    home page backend connected

--- controllers/controller.js
+++ controllers/controller.js
@@ -64,13 +64,24 @@ const loginUser = async (req, res) => {
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch)
-      return res.status(401).json({ message: "Invalid credentials" });
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid credentials" });
 
     const token = jwt.sign({ id: user._id, email: user.email }, SECRET_KEY, {
       expiresIn: "1h",
     });
 
-    res.status(200).json({ message: "Login successful", token, user });
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
