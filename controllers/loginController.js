6d95490269c9560ebd24b1188bd0c6a1d053ee49
Dilshanrad22 <dulanjanadkdh0@gmail.com>
2025-05-28 15:02:34 +0530

    update

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -78,7 +78,7 @@ const login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      maxAge: 24 * 60 * 60 * 1000,
+      maxAge: 24 * 60 * 60 * 1000   ,
       sameSite: 'strict'
     });
 
