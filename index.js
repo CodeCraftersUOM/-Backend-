59f7be3dc3558a9979ae4bd31d2b560933946fdf
Chamathka Palihawadana <palihawadanacr.22@uom.lk>
2025-05-28 13:15:51 +0530

    initial

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const express = require("express");
 const app = express();
-const cors = require("cors"); // ✅ Import cors
+const cors = require("cors");
 const model = require("./model");
 const mongoose = require("mongoose");
 const authenticationRoute = require('./routes/authenticationRoute');
-const guideRoutes = require('./routes/guideRoutes')
-const communiRoutes = require('./routes/communiRoutes')
-const repairRoutes = require('./routes/repairRoutes')
-const resturentRoutes = require('./routes/resturentRoutes')
-const healthRoutes = require('./routes/healthRoutes')
-const houeskeepingRoutes = require('./routes/houeskeepingRoutes')
-const taxiRoutes = require('./routes/taxiRoutes')
-const otherRoutes = require('./routes/otherRoutes')
-const dashboardRoutes = require('./routes/dashbordRoutes')
+const guideRoutes = require('./routes/guideRoutes');
+const communiRoutes = require('./routes/communiRoutes');
+const repairRoutes = require('./routes/repairRoutes');
+const resturentRoutes = require('./routes/resturentRoutes');
+const healthRoutes = require('./routes/healthRoutes');
+const houeskeepingRoutes = require('./routes/houeskeepingRoutes');
+const taxiRoutes = require('./routes/taxiRoutes');
+const otherRoutes = require('./routes/otherRoutes');
+const dashboardRoutes = require('./routes/dashbordRoutes');
 const cookieParser = require('cookie-parser');
-
-
 
 // ✅ Use CORS middleware
 app.use(cors({
@@ -31,57 +29,55 @@ const data = [];
 
 // Connect to Mongoose
 mongoose.connect(
-    "mongodb+srv://chandupa:81945124@cluster0.fmyrf.mongodb.net/TravelWish"
-  )
-  .then(() => {
-    console.log("Connected to MongoDB");
+  "mongodb+srv://chandupa:81945124@cluster0.fmyrf.mongodb.net/TravelWish"
+)
+.then(() => {
+  console.log("Connected to MongoDB");
 
-    app.post("/api/add_data", async (req, res) => {
-      console.log("result", req.body);
+  app.post("/api/add_data", async (req, res) => {
+    console.log("result", req.body);
 
-      let data2 = model(req.body);
-      try {
-        let dataToStore = await data2.save();
-        res.status(200).json(dataToStore);
-      } catch (error) {
-        res.status(400).json({
-          message: "Error saving data",
-          error: error.message,
-        });
-      }
-    });
-
-    app.get("/api/get_data", (req, res) => {
-      if (data.length > 0) {
-        res.status(200).send({
-          status_code: 200,
-          message: data,
-        });
-      } else {
-        res.status(404).send({
-          status_code: 404,
-          message: "No data found",
-        });
-      }
-    });
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
+    let data2 = model(req.body);
+    try {
+      let dataToStore = await data2.save();
+      res.status(200).json(dataToStore);
+    } catch (error) {
+      res.status(400).json({
+        message: "Error saving data",
+        error: error.message,
+      });
+    }
   });
+
+  app.get("/api/get_data", (req, res) => {
+    if (data.length > 0) {
+      res.status(200).send({
+        status_code: 200,
+        message: data,
+      });
+    } else {
+      res.status(404).send({
+        status_code: 404,
+        message: "No data found",
+      });
+    }
+  });
+})
+.catch((error) => {
+  console.error("Error connecting to MongoDB:", error);
+});
 
 // Web APIs
 app.use('/api', authenticationRoute);
-app.use('/api',guideRoutes);
-app.use('/api',communiRoutes);
-app.use('/api',repairRoutes);
-app.use('/api',resturentRoutes);
-app.use('/api',healthRoutes);
-app.use('/api',houeskeepingRoutes);
-app.use('/api',taxiRoutes);
-app.use('/api',otherRoutes);
-app.use('/api',dashboardRoutes)
-
-
+app.use('/api', guideRoutes);
+app.use('/api', communiRoutes);
+app.use('/api', repairRoutes);
+app.use('/api', resturentRoutes);
+app.use('/api', healthRoutes);
+app.use('/api', houeskeepingRoutes);
+app.use('/api', taxiRoutes);
+app.use('/api', otherRoutes);
+app.use('/api', dashboardRoutes);
 
 
 app.listen(2000, () => {
