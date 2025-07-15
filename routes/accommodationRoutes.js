548000500770d39a7abf25dbf30ad69bf30c610b
chandupasasmitha <chandupasasmitha@gmail.com>
2025-07-15 16:54:34 +0530

    accommodation page and details page connected with backend

--- routes/accommodationRoutes.js
+++ routes/accommodationRoutes.js
@@ -3,9 +3,13 @@ const router = express.Router();
 const {
   createAccommodationService,
   getAccommodation,
+  getAccommodationById, // ADD THIS LINE
+  searchAccommodations, // ADD THIS LINE for completeness, based on controller
 } = require("../controllers/addAccommodation");
 
 router.post("/addAccommodation", createAccommodationService);
-router.get("/getAccommodation", getAccommodation);
+router.get("/accommodations", getAccommodation);
+router.get("/accommodations/:id", getAccommodationById); // NEW ROUTE
+router.post("/accommodations/search", searchAccommodations); // NEW ROUTE for completeness
 
 module.exports = router;
