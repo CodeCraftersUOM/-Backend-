430441b3dcf431ae9e0cefb062ec90b421ae2a2c
sithika2000 <snimsilu2000k@gmail.com>
2025-07-22 23:57:51 +0530

    Restaurant created

--- routes/resturentRoutes.js
+++ routes/resturentRoutes.js
@@ -1,7 +1,18 @@
-const express = require('express')
-const router = express.Router()
-const  createResturent =require('../controllers/addresturent');
+const express = require('express');
+const router = express.Router();
+const { createRestaurant,  getRestaurant, getAllRestaurants} = require('../controllers/addresturent');
 
-router.post('/addresturent',createResturent.createRestaurantService);
+
+// Create a new restaurant with validation
+router.post('/addRestaurant', createRestaurant);
+
+// Get single restaurant (no validation needed)
+router.get('/restaurant/:id', getRestaurant);
+
+// Get all restaurants (no validation needed)
+router.get('/restaurants', getAllRestaurants);
+
+// Delete restaurant (only need to validate ID)
+
 
 module.exports = router;
