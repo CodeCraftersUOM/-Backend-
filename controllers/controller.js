720fbfdecaac47a526aa7b57e9e6f722ef637f37
chandupasasmitha <chandupasasmitha@gmail.com>
2025-07-31 09:42:17 +0530

    user already exist fixed

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,20 +1,34 @@
-const { response } = require("../app");
 const User = require("../models/model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
+// Use a secure, environment-specific secret key in a real application
+const SECRET_KEY = "yoursecretkey";
+
+// GET ALL USERS
 const getUsers = (req, res, next) => {
   User.find()
     .then((response) => {
       res.json({ response });
     })
     .catch((error) => {
-      res.json({ error: error });
+      res.json({ error: error.message });
     });
 };
 
-const bcrypt = require("bcrypt");
-
+// ADD A NEW USER (SIGN UP)
 const addUser = async (req, res) => {
   try {
+    // Check if a user with the same username already exists
+    const existingUser = await User.findOne({ username: req.body.username });
+    if (existingUser) {
+      // If user exists, return a 409 Conflict status
+      return res
+        .status(409)
+        .json({ success: false, message: "Username already exists" });
+    }
+
+    // Hash the password before saving
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     const user = new User({
       username: req.body.username,
@@ -23,61 +37,48 @@ const addUser = async (req, res) => {
     });
 
     const savedUser = await user.save();
-    res
-      .status(200)
-      .json({ message: "User created", user: savedUser, test: "test" });
+
+    // Respond with a 201 Created status for success
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: {
+        id: savedUser._id,
+        username: savedUser.username,
+        email: savedUser.email,
+      },
+    });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ success: false, message: error.message });
   }
 };
 
-const updateUser = (req, res, next) => {
-  const id = req.body.id;
-  const username = req.body.username;
-  User.updateOne({ id: id }, { $set: { username: username } })
-    .then((response) => {
-      res.json({ response });
-    })
-    .catch((error) => {
-      res.json({ error: error });
-    });
-};
-
-const deleteUser = (req, res, next) => {
-  const id = req.body.id;
-  User.deleteOne({ id: id })
-    .then((response) => {
-      res.json({ response });
-    })
-    .catch((error) => {
-      res.json({ error: error });
-    });
-};
-
-const jwt = require("jsonwebtoken");
-const SECRET_KEY = "yoursecretkey"; // Use .env file in real apps
-
+// LOGIN USER
 const loginUser = async (req, res) => {
   try {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
-    console.log("User found:", user); // Debug
 
-    if (!user) return res.status(404).json({ message: "User not found" });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
 
+    // Compare the submitted password with the hashed password in the database
     const isMatch = await bcrypt.compare(password, user.password);
-    console.log("Password match:", isMatch); // Debug
-
-    if (!isMatch)
+    if (!isMatch) {
       return res
         .status(401)
         .json({ success: false, message: "Invalid credentials" });
+    }
 
+    // If credentials are correct, create a JWT
     const token = jwt.sign(
       { id: user._id, username: user.username },
       SECRET_KEY,
       {
-        expiresIn: "1h",
+        expiresIn: "1h", // Token will expire in 1 hour
       }
     );
 
@@ -92,12 +93,37 @@ const loginUser = async (req, res) => {
       },
     });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ success: false, message: error.message });
   }
 };
 
-exports.loginUser = loginUser;
+// UPDATE USER (Example - implementation may vary)
+const updateUser = (req, res, next) => {
+  const { id, username } = req.body; // Assuming you pass user ID and new username
+  User.updateOne({ _id: id }, { $set: { username: username } })
+    .then((response) => {
+      res.json({ response });
+    })
+    .catch((error) => {
+      res.json({ error: error.message });
+    });
+};
+
+// DELETE USER (Example - implementation may vary)
+const deleteUser = (req, res, next) => {
+  const { id } = req.body; // Assuming you pass user ID
+  User.deleteOne({ _id: id })
+    .then((response) => {
+      res.json({ response });
+    })
+    .catch((error) => {
+      res.json({ error: error.message });
+    });
+};
+
+// Export all controller functions
 exports.getUsers = getUsers;
 exports.addUser = addUser;
+exports.loginUser = loginUser;
 exports.updateUser = updateUser;
 exports.deleteUser = deleteUser;
