4c453659ffc64bc3eb7a92d7dfcff15ed8469d88
Chamathka Palihawadana <palihawadanacr.22@uom.lk>
2025-07-31 05:53:51 +0530

    card routes

--- routes/cardRoutes.js
+++ routes/cardRoutes.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const { createCardDetail } = require("../controllers/addcard");
 
-// Example POST endpoint for card details
-router.post("/createCardDetail", async (req, res) => {
-  // TODO: Save card details to DB (add validation and security!)
-  res.status(200).json({ message: "Card details received" });
-});
+// Update the route to match frontend URL
+router.post("/cards", createCardDetail);
 
 module.exports = router;
